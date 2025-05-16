b894d027716d9ffe4f98c26ecbf480a54c5a02a1
Daniel Diaz Martinez <diazmartinezdaniel30@gmail.com>
2025-05-16 22:29:53 +0200

    feat: Enhance admin dashboard with user, subject, and document management links; update styles for better UI

--- frontend/src/app/features/admin-view/components/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/features/admin-view/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../../core/services/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss']
 })
